3d778bf378fe27996c6989f8084b9328b2377206
alvin4032 <alvinamaterasu@gmail.com>
2022-02-08 14:29:09 +0700

    Regresi Logistik di R

---
+++ 2.1 Regresi Logistik di R.R
@@ -0,0 +1,50 @@
+# Logistic Regression
+# sumber : http://www.sthda.com/english/articles/36-classification-methods-essentials/151-logistic-regression-essentials-in-r/
+
+library(tidyverse)
+library(caret)
+theme_set(theme_bw())
+
+# load dataset
+data("PimaIndiansDiabetes2", package = "mlbench")
+df <- PimaIndiansDiabetes2
+
+# Membersihkan data NA
+df <- na.omit(df)
+
+# Check dataframe
+class(df)
+summary(df)
+
+# Memisahkan data training dan data set
+set.seed(40)
+df.sample <- df$diabetes %>% createDataPartition(p = 0.8, list = F)
+data.train <- df[df.sample, ]
+data.test <- df[-df.sample, ]
+
+# Membuat model
+model <- glm( diabetes ~., data = data.train, family = binomial())
+
+summary(model)
+
+# Membuat probebilitas
+probabilitas <- model %>% predict(data.test, type = "response")
+prediksi.class <- ifelse(probabilitas> 0.5, "pos", "neg")
+
+# Akurasi Model
+mean(prediksi.class == data.test$diabetes)
+
+model <- glm( diabetes ~ glucose, data = train.data, family = binomial)
+summary(model)$coef
+
+# plot
+data.train %>%
+  mutate(probabilitas = ifelse(diabetes == "pos", 1,0)) %>%
+  ggplot(aes(glucose, probabilitas)) +
+  geom_point(aplha = 0.2) +
+  geom_smooth(method = "glm", method.args = list(family = "binomial")) +
+  labs(
+    title = "Model Regresi Logistik",
+    x = "Konsentrasi Glukosa Plasma",
+    y = "Probabilitas positif diabetes"
+  )
